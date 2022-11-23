858ee3259858626e1cf3d3c3b206673141c67639
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-11-23 15:48:26 +0100

    movies api delete

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::MoviesController < ApplicationController
     @movies = Movie.all
     # render json: @movies
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+    render json: @movie
+  end
+
+  def delete
+    movie = Movie.find(params[:id])
+    movie.delete
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :movies, only: [:index]
+      resources :movies do
+        member do
+          delete :delete
+        end
+      end
     end
   end
   # Defines the root path route ("/")
